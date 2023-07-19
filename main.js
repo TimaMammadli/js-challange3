2fde5bd13ef67f85253004a4c9ca17d362495a1d
Teymur <m.teymur2003@gmail.com>
2023-07-19 16:19:40 +0400

    Tima

---
+++ main.js
@@ -0,0 +1,73 @@
+                    // 1
+// let a=3;
+// let result = Array.isArray(a);
+// console.log(result);
+                  
+ 
+                    //   2 
+// numbers=[1,2,3,4,5];
+// numbersCopy = Array.from(numbers);
+// console.log(numbersCopy);
+
+
+                    //   3      
+// let phone = '088433'.replace(/^(\d{1})(\d{3})(\d{2})/, '$1-$2-$3')
+// console.log(phone)
+
+
+                    //  4
+// var a=[1,2,3], b=[101,1,2,10]
+// var c= a.concat(b.filter((item) => a.indexOf(item) < 0))
+// console.log(c);
+
+
+                //    5
+// const array =[2,5,9,6];
+// console.log(array);
+// const index = array.indexOf(5);
+// if (index > -1);{
+//     array.splice(index ,1 );
+// } 
+// console.log(array);/
+
+
+                //    6
+// function removeDuplicates(array) {
+
+//     const uniqueSet = new Set(array);
+
+//     const result = Array.from(uniqueSet);
+  
+//     return result;
+//   }
+  
+//   const inputArray = [1, 2, 2, 3, 4, 4, 5];
+//   const outputArray = removeDuplicates(inputArray);
+//   console.log(outputArray); 
+  
+
+
+
+                    //   7
+// function isTargetPresent(array, target) {
+//     return array.includes(target);
+//   }
+//   const numbers = [1, 2, 3, 4, 5];
+//   const targetNumber = 4;
+  
+//   console.log(isTargetPresent(numbers, targetNumber));
+  
+
+ 
+                    // 8
+// function findCommonElements(array1, array2) {
+//     const commonElements = array1.filter(element => array2.includes(element));
+  
+//     return commonElements;
+//   }
+//   const array1 = [1, 2, 3, 4, 5];
+//   const array2 = [4, 5, 6, 7, 8];
+  
+//   const commonArray = findCommonElements(array1, array2);
+//   console.log(commonArray); 
+  
